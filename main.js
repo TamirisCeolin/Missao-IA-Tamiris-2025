527d9634dc4ad9f9eddf55e0b530b911e6b6b62b
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2025-09-28 17:50:39 -0300

    28/09

--- main.js
+++ main.js
@@ -88,7 +88,7 @@ function mostraPergunta() {
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener('click', function () {
             atual++;
             mostraPergunta();
