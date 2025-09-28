cbefb508754c14923cb84fe9a6040e42ae622442
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2025-09-28 19:03:11 -0300

    28/09

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ function mostraAlternativas() {
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacoes;
+    const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
